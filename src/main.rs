9808e0c28362f6b0f23d2a09e613d875f9e7823a
1MaxKoval <koval6813@gmail.com>
2023-07-14 16:43:23 +0200

    Did some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,53 @@
 use std::collections::{HashMap, BinaryHeap};
 use std::cmp::Reverse;
 use std::cmp::{Ord, Ordering};
+use std::fs::File;
+use std::io::prelude::*;
+
+struct HuffCodeString {
+    data: Vec<u8>,
+    bit_pointer: u8,
+    bits_set: u8,
+    elem_pointer: usize,
+}
+
+impl HuffCodeString {
+
+    fn new() -> Self {
+        return HuffCodeString {
+            data: vec![0],
+            bit_pointer: 128, // 10000000
+            elem_pointer: 0,
+            bits_set: 0
+        };
+    }
+
+    fn set_bit(&mut self, b: bool) {
+        if b {
+            let mut v = self.data[self.elem_pointer];
+            self.data[self.elem_pointer] = v | self.bit_pointer
+        }
+        if self.bit_pointer == 1 { // Reached 0000001
+            self.bit_pointer = 128;
+            self.elem_pointer += 1;
+            self.data.push(0);
+        }
+        else { self.bit_pointer = self.bit_pointer >> 1; }
+    }
+
+    fn consume(code_string: HuffCodeString) {
+
+    }
+
+}
+
+impl Iterator for HuffCodeString {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+    }
+
+}
 
 struct Node<T> {
     left: Option<Box<Node<T>>>,
@@ -43,6 +90,15 @@ impl<T> PartialEq for Node<T> {
 
 impl<T> Eq for Node<T> {}
 
+fn write_bytes(data: &Vec<u8>, file_path: &str) {
+    let mut f = File::create(file_path)
+        .expect("Couldnt read create a file");
+    f.write_all(data).expect("Couldnt write to file");
+}
+
+fn get_coding_scheme(head_node: Node<char>, data: &str) -> HashMap<char, usize> {
+
+}
 
 fn construct_huffman_tree(f: &HashMap<char, usize>) -> Node<char> {
     let mut heap = BinaryHeap::new();
@@ -58,7 +114,7 @@ fn construct_huffman_tree(f: &HashMap<char, usize>) -> Node<char> {
             )
         ) });
     for _ in 0..heap.len() {
-        let Reverse(first_node )= heap.pop().unwrap();
+        let Reverse(first_node) = heap.pop().unwrap();
         let Reverse(second_node) = heap.pop().unwrap();
         let sum = first_node.weight + second_node.weight;
         heap.push(
@@ -77,5 +133,4 @@ fn construct_huffman_tree(f: &HashMap<char, usize>) -> Node<char> {
 }
 
 fn main() {
-    println!("Hello, world!");
 }
